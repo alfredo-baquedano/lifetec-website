ac6c0d467f5d79d36427c392192fcc86a5393234
abaquedano@acid.cl <alfredo.baquedano.c@gmail.com>
2023-06-08 21:54:11 -0400

    Change contact section and remove unused contact forms

--- php/contactform-process.php
+++ php/contactform-process.php
@@ -19,14 +19,8 @@ if (empty($_POST["message"])) {
     $message = $_POST["message"];
 }
 
-if (empty($_POST["terms"])) {
-    $errorMSG = "Terms is required ";
-} else {
-    $terms = $_POST["terms"];
-}
-
-$EmailTo = "yourname@domain.com";
-$Subject = "New message from Aria landing page";
+$EmailTo = "contacto@lifetec.cl";
+$Subject = "Nuevo mensaje desde lifetec.cl";
 
 // prepare email body text
 $Body = "";
@@ -38,9 +32,6 @@ $Body .= $email;
 $Body .= "\n";
 $Body .= "Message: ";
 $Body .= $message;
-$Body .= "\n";
-$Body .= "Terms: ";
-$Body .= $terms;
 $Body .= "\n";
 
 // send email
